aa45d938f04987c61c452d18f79242dead3accd8
Márcia Ferreira <marciasilvacf1989@gmail.com>
2025-01-23 19:20:54 -0300

    atividades conex html

---
+++ prozconexHtml/conect.js
@@ -0,0 +1,18 @@
+//Atividade : 
+//Crie dois arquivos, um arquivo HTML com uma estrutura HTML e um h1 com o texto 
+//"Conexão com um arquivo JavaScript", e um arquivo
+//JS com uma função que imprima no console a frase "Conexão feita com sucesso!" e execute três vezes essa função, 
+// depois link o arquivo JS no arquivo HTML e e verifique se a frase "Conexão feita com sucesso!" 
+// é impressa três vezes no console.
+
+
+//resolvi fazer no formato Arrow function para testar a forma de execução.
+
+const conexao = () => {
+    console.log("Conexão feita com sucesso!");
+}
+
+conexao()
+conexao()
+conexao()
+
